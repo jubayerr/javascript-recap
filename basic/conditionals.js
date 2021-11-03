b3cd10d9b8398209bf5af954432104e1e27bf8ad
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-03 21:21:49 +0600

    conditionals

---
+++ basic/conditionals.js
@@ -0,0 +1,11 @@
+// conditionals in JS
+
+var burgerPrice = 225;
+
+if (burgerPrice <= 250) {
+  console.log("I will eat the Burger!");
+} else {
+  ("I will try another food.");
+}
+
+// ### end code ###
