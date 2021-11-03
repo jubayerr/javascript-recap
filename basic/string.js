40720ef99784e097f8b95de327988ed7cc492849
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-03 19:42:29 +0600

    stirng

---
+++ basic/string.js
@@ -0,0 +1,22 @@
+// ### string ###
+
+var typingTest = "A brown FOX jumps over the lazy dog quickly";
+
+// string methods
+
+var lowerString = typingTest.toLowerCase();
+
+var upperString = typingTest.toUpperCase();
+
+var findString = typingTest.indexOf("dog");
+
+var splitString = typingTest.split(" ");
+
+// outputs
+
+console.log(lowerString);
+console.log(upperString);
+console.log(findString);
+console.log(splitString);
+
+// ### end code ###
