f1f361b1740e11133c298b162d1a0c5f27ed67bf
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-03 22:15:01 +0600

    conditions

--- basic/conditionals.js
+++ basic/conditionals.js
@@ -1,11 +1,14 @@
 // conditionals in JS
 
-var burgerPrice = 225;
+var burgerPrice = 150;
+var isFriday = false;
 
-if (burgerPrice <= 250) {
-  console.log("I will eat the Burger!");
+if (burgerPrice <= 250 && isFriday == true) {
+  console.log("I will eat the Burger and Chicken.");
+} else if (burgerPrice <= 150 && isFriday == false) {
+  console.log("I will eat just Burger.");
 } else {
-  ("I will try another food.");
+  console.log("I will eat just Chicken.");
 }
 
 // ### end code ###
