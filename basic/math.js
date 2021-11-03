449cdd839cbe8c7ced6bdb76db15cdadf06593d3
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-03 20:38:10 +0600

    mathmatical operation

---
+++ basic/math.js
@@ -0,0 +1,17 @@
+// mathematical operation in JS
+
+var price1 = 25;
+var price2 = 15;
+
+price1++; // add 1
+price2--; // minus 1
+
+var total = price1 + price2;
+// var total = price1 - price2;
+// var total = price1 * price2;
+// var total = price1 / price2;
+// var total = price1 % price2; // modules (reminder of division)
+
+console.log(total);
+
+// ### end code ###
