e56ede436830f555cadfa1ee2374661de1511377
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-03 18:32:46 +0600

    variables

--- basic/variables.js
+++ basic/variables.js
@@ -1,5 +1,10 @@
 // variables
 
 var myName = "Jubayer";
+var myAge = 24;
 
-console.log(myName);
+var myProfile = `My name is ${myName}. I'm ${myAge} years old.`;
+
+console.log(myProfile);
+
+// ### end ###
