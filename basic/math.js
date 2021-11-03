a4a8c719d3b66de3848c819a1faac7a14ea8554c
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-03 20:55:15 +0600

    mathematical operation

--- basic/math.js
+++ basic/math.js
@@ -14,4 +14,23 @@ var total = price1 + price2;
 
 console.log(total);
 
+// another operation
+
+var balance1 = -5;
+var balance2 = 20.34;
+
+var absoluteNumber = Math.abs(balance1);
+var roundNumber = Math.round(balance2);
+var ceilingNumber = Math.ceil(balance2);
+var floorNumber = Math.floor(balance2);
+
+var randomNumber = Math.random() * 10;
+var randomResult = Math.round(randomNumber);
+
+console.log(absoluteNumber);
+console.log(roundNumber);
+console.log(ceilingNumber);
+console.log(floorNumber);
+console.log(randomResult);
+
 // ### end code ###
