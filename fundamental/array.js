79b8ff0f7a57a4414b91f90061c12e7000510495
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-04 11:14:27 +0600

    arrays

--- fundamental/array.js
+++ fundamental/array.js
@@ -26,7 +26,30 @@ console.log(totalFriends);
 friendsAge.pop();
 console.log(friendsAge);
 
-friendsAge.shift();
-console.log(friendsAge);
+// add & remove element from the beginning and slice
 
-// end code
+var cast = ["Jonas", "Bartosz", "Magnus", "Mikkel"];
+
+cast.push("Martha"); // add element in the last
+
+console.log(cast);
+
+cast.shift(); // remove element from the beginning
+
+cast.unshift("Franziska"); // add element at the beginning
+
+console.log(cast);
+
+var slicedCast = cast.slice(2, 5); // take element from array
+
+console.log(slicedCast);
+
+console.log(cast);
+
+var splicedCast = cast.splice(2, 4); // cut element from array
+
+console.log(splicedCast);
+
+console.log(cast);
+
+// ### end code ###
