669ff293dba1b88094feaf3175a941b4fed40ee4
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-04 17:20:29 +0600

    loops

---
+++ fundamental/loops.js
@@ -0,0 +1,21 @@
+// loop
+
+var num = 0;
+
+// while loop
+
+while (num <= 10) {
+  console.log(num);
+  num++;
+}
+
+// for loop
+
+var numbers = [22, 33, 44, 55, 66, 77, 88];
+
+for (let i = 0; i <= numbers.length; i++) {
+  var element = numbers[i];
+  console.log(element);
+}
+
+// ### end code ###
