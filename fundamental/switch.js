6506249f9258f005df7873da818a22ece18f6a37
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-06 10:09:08 +0600

    switch

---
+++ fundamental/switch.js
@@ -0,0 +1,27 @@
+// switch in JS
+
+var car = "tesla";
+
+switch (car) {
+  case "bmw":
+    console.log("bmw is great");
+    break;
+  case "ford":
+    console.log("ford is good");
+    break;
+  case "tesla":
+    console.log("tesla is my favorite");
+    break;
+  case "porsche":
+    console.log("porsche is best");
+    break;
+  case "jaguar":
+    console.log("jaguar is good");
+    break;
+
+  default:
+    console.log("None brand car");
+    break;
+}
+
+// ### end code ###
