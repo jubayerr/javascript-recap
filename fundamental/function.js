dbb0183ef3d21356feed508dcc26a10d86a47b75
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-06 10:38:03 +0600

    function

--- fundamental/function.js
+++ fundamental/function.js
@@ -1,13 +1,17 @@
 // function is JS
 
 var number = 5;
+var country = "Bangladesh";
 
-function sayHello(n) {
+function sayHello(n, c) {
   for (let i = 0; i < n; i++) {
-    console.log("Hello Bangladesh! Good Morning.");
+    var greeting = `Good Morning, ${c}!`;
+    return greeting;
   }
 }
 
-sayHello(number);
+var greetBangladesh = sayHello(number, country);
+
+console.log(greetBangladesh);
 
 // ### end code ###
