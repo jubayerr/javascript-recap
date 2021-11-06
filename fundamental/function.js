00e7493ee09637463805c9a55df383e03be9dc43
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-06 10:22:00 +0600

    function

---
+++ fundamental/function.js
@@ -0,0 +1,13 @@
+// function is JS
+
+var number = 5;
+
+function sayHello(n) {
+  for (let i = 0; i < n; i++) {
+    console.log("Hello Bangladesh! Good Morning.");
+  }
+}
+
+sayHello(number);
+
+// ### end code ###
