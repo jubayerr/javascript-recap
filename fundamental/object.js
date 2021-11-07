30003b0b684a277b55666907077de6eccbf7cf92
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-07 12:05:02 +0600

    object

---
+++ fundamental/object.js
@@ -0,0 +1,24 @@
+// object in JS
+
+var student1 = {
+  name: "Jonas",
+  id: 321456,
+  age: 24,
+  isStudent: true,
+};
+
+var student2 = {
+  name: "Martha",
+  id: 321455,
+  age: 24,
+  isStudent: true,
+};
+
+student1.phone = 0123456;
+
+student2.id = 321450; // update property
+
+console.log(student1);
+console.log(student2.id);
+
+// ## end code ##
