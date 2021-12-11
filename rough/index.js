a10643f8b10f4b0b39a30521321979cfd05b74a5
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-12-11 20:09:48 +0600

    js practice

---
+++ rough/index.js
@@ -0,0 +1,13 @@
+var myName = "Jubayer";
+var myAge = 24;
+
+var profile = `My name is ${myName} and I am ${myAge} years old.`;
+
+console.log(profile);
+
+for (let i = 0; i < 10; i++) {
+  var randomNum = Math.random() * 6;
+
+  var result = Math.ceil(randomNum);
+  console.log(result);
+}
