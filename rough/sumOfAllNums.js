b38894f64b56f5a5c0aa962a0738d2972ab3dc40
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-01-15 18:38:19 +0600

    sum of all number in array

---
+++ rough/sumOfAllNums.js
@@ -0,0 +1,12 @@
+// Sum of all numbers in array
+
+const allMarks = [33, 44, 55, 66, 77, 88];
+
+let sumOfAllMarks = 0;
+
+for (let i = 0; i < allMarks.length; i++) {
+  const element = allMarks[i];
+  sumOfAllMarks += element;
+}
+
+console.log(sumOfAllMarks);
