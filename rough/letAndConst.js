f2a1b92b16427d1f0383e247a5a9d87f4126b6f5
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-01-24 10:02:23 +0600

    js recap

---
+++ rough/letAndConst.js
@@ -0,0 +1,15 @@
+// var, let and const
+
+var myName = "Jubayer";
+let myAge = 24;
+const myCountry = "Bangladesh";
+
+if (myName.length > 5) {
+  myName = "Jonas";
+}
+
+myAge = 25;
+
+console.log(myName);
+console.log(myAge);
+console.log(myCountry);
