d5f27cadda9ff0e821f7e3e3d13dcaba9e4c9510
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-01-24 13:58:12 +0600

    create fibonacci recap

--- rough/fiboSeries.js
+++ rough/fiboSeries.js
@@ -1,1 +1,17 @@
 // create fibonacci series
+
+function fibonacciSeries(n) {
+  var fibo = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+  return fibo;
+}
+
+var result = fibonacciSeries(55);
+
+for (let i = 0; i < result.length; i++) {
+  const element = result[i];
+  console.log(element);
+}
