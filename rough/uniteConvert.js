aabc0c773f7f3490730e9c802b245f9b39daa434
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-01-24 09:44:08 +0600

    js recap

--- rough/uniteConvert.js
+++ rough/uniteConvert.js
@@ -1,4 +1,8 @@
+// convert inch to feet
+
 var tableSize = 50;
+var doorSize = 60;
+var roomSize = 156;
 
 function converter(inch) {
   var feet = inch / 12;
@@ -6,5 +10,11 @@ function converter(inch) {
 }
 
 var result = converter(tableSize);
+var result2 = converter(doorSize);
+var result3 = converter(roomSize);
+var result4 = converter(228);
 
-console.log(result.toFixed(2));
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(result4);
