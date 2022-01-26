ef06e783e6db2fa487f54cca3b55758184484aed
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-01-26 08:40:47 +0600

    dice algorithm

--- coding-problem-recap/randomdice.js
+++ coding-problem-recap/randomdice.js
@@ -4,4 +4,28 @@ var randomNum = Math.random() * 6;
 
 var randomDice = Math.ceil(randomNum);
 
-console.log(randomDice);
+// console.log(randomDice);
+
+switch (randomDice) {
+  case 1:
+    console.log("one");
+    break;
+  case 2:
+    console.log("two");
+    break;
+  case 3:
+    console.log("three");
+    break;
+  case 4:
+    console.log("four");
+    break;
+  case 5:
+    console.log("five");
+    break;
+  case 6:
+    console.log("six");
+    break;
+  default:
+    console.log("something wrong");
+    break;
+}
