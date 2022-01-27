570d2977d2f6db5d46d16bca0b295baf99a2d57c
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-01-27 08:48:34 +0600

    js problem solving

---
+++ practice-problem-recap/countingAnimal.js
@@ -0,0 +1,23 @@
+// counting wild animal in jungle
+
+function animalCounter(depth) {
+  var animal = 0;
+  if (depth <= 10) {
+    animal = depth * 50;
+  } else if (depth <= 20) {
+    var firstPart = 10 * 50;
+    var remaining = depth - 10;
+    var secondPart = remaining * 100;
+    animal = firstPart + secondPart;
+  } else {
+    var firstPart = 10 * 50;
+    var secondPart = 10 * 100;
+    var remaining = depth - 20;
+    var thirdPart = remaining * 300;
+    animal = firstPart + secondPart + thirdPart;
+  }
+  return animal;
+}
+
+var result = animalCounter(50);
+console.log(result);
