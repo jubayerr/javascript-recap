b393b9a97d6a0b0f2a86037ea048e1af547f58af
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-01-29 11:25:21 +0600

    js dom recap

---
+++ js-dom/script.js
@@ -0,0 +1,13 @@
+const mainArea = document.getElementById("main-area");
+
+document.body.style.backgroundColor = "lightGray";
+mainArea.style.backgroundColor = "black";
+mainArea.style.textAlign = "center";
+mainArea.innerHTML = `
+<h1>JavaScript DOM</h1>
+<p>These text are came from javascript</p>
+`;
+mainArea.style.color = "white";
+mainArea.style.padding = "20px";
+mainArea.style.margin = "20px";
+mainArea.style.borderRadius = "20px";
